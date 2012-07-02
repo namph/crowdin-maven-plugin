2034ef5fcce227b8eff1106fd30bf3b1fefa9843
viet nguyen <vietnt@exoplatform.com>
2012-07-02 11:21:48 +0700

    PLFENG-464: Add Commons and Integration locales

--- scripts/post-process.sh
+++ scripts/post-process.sh
@@ -23,8 +23,8 @@
 # eXoProjects directory 
 EXO_PROJECTS=`pwd`
 
-projects=( 'platform' 'ecms' 'cs' 'ks' 'social' 'exogtn' 'webos')
-versions=( '3.5.x' '2.3.x' '2.2.x' '2.2.x' '1.2.x' '3.2.x' '2.0.x' )
+projects=( 'platform' 'ecms' 'cs' 'ks' 'social' 'commons' 'integration' 'exogtn' 'webos')
+versions=( '3.5.x' '2.3.x' '2.2.x' '2.2.x' '1.2.x' '1.1.x' '1.0.x' '3.2.x' '2.0.x' )
 length=${#projects[@]}
 
 echo "=========================Restoring projects structure========================="
