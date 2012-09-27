e9ba900c852f461032d09c143f01f82f5c3774ce
Tung Vu Minh <tungvm@exoplatform.com>
2012-09-27 17:48:48 +0700

    PLFENG-931: Unify projects declaration into projects.sh

--- scripts/post-process.sh
+++ scripts/post-process.sh
@@ -19,12 +19,12 @@
 # Purpose : Restore projects structure
 #
 
+# import projects declaration
+source "../projects.sh"
 
 # eXoProjects directory 
 EXO_PROJECTS=`pwd`
 
-projects=( 'platform' 'ecms' 'cs' 'ks' 'social' 'commons' 'integration' 'exogtn' 'webos')
-versions=( '3.5.x' '2.3.x' '2.2.x' '2.2.x' '1.2.x' '1.1.x' '1.0.x' '3.2.x' '2.0.x' )
 length=${#projects[@]}
 
 echo "=========================Restoring projects structure========================="
--- scripts/update.sh
+++ scripts/update.sh
@@ -19,13 +19,12 @@
 # Purpose : Prepare projects structure, clone projects if it is not existed, create branches from previous tags and switch to these branches
 #
 
+# import projects declaration
+source "../projects.sh"
 
 # eXoProjects directory 
 EXO_PROJECTS=`pwd`
 
-projects=( 'platform' 'ecms' 'cs' 'ks' 'social' 'commons' 'integration' 'exogtn' 'webos' )
-versions=( '3.5.x' '2.3.x' '2.2.x' '2.2.x' '1.2.x' '1.1.x' '1.0.x' '3.2.x' '2.0.x')
-oldtags=( '3.5.3' '2.3.7' '2.2.9' '2.2.9' '1.2.9' '1.1.8' '1.0.7' '3.2.4-PLF' '2.0.4' )
 length=${#projects[@]}
 
 echo "=========================Preparing projects structure========================="
