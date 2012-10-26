fc09075a82813e158db70fbe1fc74d87e90b43b0
Tung Vu Minh <tungvm@exoplatform.com>
2012-10-26 16:26:30 +0700

    PLFENG-1011: Remove double-quote and comma (,) escaping caused by Apache PropertiesConfiguration lib

--- scripts/per-file-processing.sh
+++ scripts/per-file-processing.sh
@@ -1,8 +1,8 @@
 FILENAME=$1
-#echo "Processing $FILENAME..."
+echo "Processing $FILENAME..."
 
 # Restoring escaped characters (:#!=)
-sed -i -e 's|\\\([:#!=/]\)|\1|g' $FILENAME
+sed -i -e 's|\\\([:#!=/,"]\)|\1|g' $FILENAME
 
 # Convert unicode characters to native
 #native2ascii -encoding UTF8 -reverse $FILENAME $FILENAME
